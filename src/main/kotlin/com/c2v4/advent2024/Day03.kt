8c28b8351c9280fe3ce8e3e6ff329858692b1de2
Mateusz Duda <c2v4@users.noreply.github.com>
2024-12-03 12:13:39 +0100

    Is there an award for this?

--- src/main/kotlin/com/c2v4/advent2024/Day03.kt
+++ src/main/kotlin/com/c2v4/advent2024/Day03.kt
@@ -1,18 +1,10 @@
 package com.c2v4.advent2024
 
-import kotlin.math.abs
-
-// find all matches of regex mul\((\d{1,3}),(\d{1,3})\) and return the sum of x*y
 fun mull(input: String): Int = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)").findAll(input).map {
     it.groupValues[1].toInt() * it.groupValues[2].toInt()
 }.sum()
 
-//remove from string parts between "don't()" and ("do()" or end of the string) and then find all matches of regex mul\((\d{1,3}),(\d{1,3})\) and return the sum of x*y
-fun mull2(input: String): Int = Regex("don't\\(\\)(.*?)(?=do\\(\\)|\$)").replace(input.replace("\n",""), "").let {
-    Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)").findAll(it).map {
-        it.groupValues[1].toInt() * it.groupValues[2].toInt()
-    }.sum()
-}
+fun mull2(input: String): Int = mull(Regex("don't\\(\\)(.*?)(?=do\\(\\)|\$)").replace(input.replace("\n", ""), ""))
 
 fun main(args: Array<String>) {
     println(mull("day03.txt".asResource()))
